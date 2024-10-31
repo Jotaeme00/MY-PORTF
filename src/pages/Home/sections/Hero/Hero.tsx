9283dc17f54c8e6cdc5e2bdb9e9f0a04d3555777
Juan Muniz <juan@leao.com>
2024-10-31 18:08:17 -0300

    styling my project theme and adding the button component

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,18 +1,17 @@
 import styled from "@emotion/styled"
 import Avatar  from "../../../../assets/images/avatar.jpg"
 import Grid from "@mui/material/Grid"
-import  Container  from "@mui/material/Container"
+import Container  from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
 import Button  from "@mui/material/Button"
 
-
 const Hero = () => {
   
-    const StyledHero = styled("div")(()=> ({    
+    const StyledHero = styled("div")(({theme})=> ({    
         backgroundColor: "black",
-        height: "100vh"
+        height: "100vh",
     }))
 
     const StyledImg = styled("img")(()=> ({    
@@ -30,9 +29,9 @@ const Hero = () => {
                 <StyledImg src={Avatar} />
             </Grid>
             <Grid item xs={12} md={8}>
-                <Typography color = "red" variant="h1" textAlign="center">Juan Muniz</Typography>
-                <Typography color = "red" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
-                <Grid container display="flex" justifyContent="center">
+                <Typography color = "primary" variant="h1" textAlign="center">Juan Muniz</Typography>
+                <Typography color = "primary" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
+            <Grid container display="flex" justifyContent="center">
                     <Grid item xs={12} md={4} display="flex" justifyContent="center"> 
                         <Button>
                             <DownloadIcon />    
